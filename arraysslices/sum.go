37588e779593a6c1fefc222b9cde48339aa49ea0
cooperbraun13 <cooperjbraun13@gmail.com>
2025-12-30 22:08:23 -0800

    feat: finished arrays and slices chapter

--- arraysslices/sum.go
+++ arraysslices/sum.go
@@ -7,3 +7,27 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
+
+func SumAllHeads(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		head := numbers[:1]
+		sums = append(sums, Sum(head))
+	}
+
+	return sums
+}
