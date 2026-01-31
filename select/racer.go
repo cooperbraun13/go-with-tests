07e4bed20bed5ead30d7c688c790997a94f76541
cooperbraun13 <cooperjbraun13@gmail.com>
2026-01-31 02:12:05 -0800

    feat: almost done with select chapter

--- select/racer.go
+++ select/racer.go
@@ -2,15 +2,14 @@ package racer
 
 import (
 	"net/http"
-	"time"
 )
 
-func Racer(a, b string) (winner string) {
+func Racer(a, b string) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
 	}
 }
 
@@ -21,10 +20,4 @@ func ping(url string) chan struct{} {
 		close(ch)
 	}()
 	return ch
-}
-
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
 }
